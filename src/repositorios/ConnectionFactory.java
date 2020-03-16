31843437aab405def81ef9108d5ffe7347f2a1b6
Róger Luiz <rlom@cin.ufpe.br>
2020-03-16 03:21:50 -0300

    Ajustes para Heroku

--- src/repositorios/ConnectionFactory.java
+++ src/repositorios/ConnectionFactory.java
@@ -6,12 +6,13 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 	public Connection getConnection() {
-        try {
-        	Class.forName("com.mysql.jdbc.Driver");
-            return DriverManager.getConnection(
-                    "jdbc:mysql://localhost/userView", "root", "admin");
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection(
+					"mysql://adffdadf2341:adf4234@us-cdbr-east.cleardb.com/heroku_db?reconnect=true", "b92da99c11ce75",
+					"f5fdcb75");
+		} catch (SQLException | ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
