7ae194d411a3d159b8a062bc88955af85ce2815a
Róger Luiz <rlom@cin.ufpe.br>
2020-03-16 03:25:47 -0300

    BD Name Heroku

--- src/repositorios/ConnectionFactory.java
+++ src/repositorios/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection(
-					"mysql://adffdadf2341:adf4234@us-cdbr-east.cleardb.com/heroku_db?reconnect=true", "b92da99c11ce75",
+					"mysql://CLEARDB_DATABASE_URL?reconnect=true", "b92da99c11ce75",
 					"f5fdcb75");
 		} catch (SQLException | ClassNotFoundException e) {
 			throw new RuntimeException(e);
