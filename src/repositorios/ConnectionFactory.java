759f28c6862a62756a28bce413f6fc52c3415840
Róger Luiz <rlom@cin.ufpe.br>
2020-03-16 21:51:02 -0300

    Ajustes na Configuração do Projeto

--- src/repositorios/ConnectionFactory.java
+++ src/repositorios/ConnectionFactory.java
@@ -9,8 +9,11 @@ public class ConnectionFactory {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection(
-					"mysql://CLEARDB_DATABASE_URL?reconnect=true", "b92da99c11ce75",
-					"f5fdcb75");
+					"jdbc:mysql://b92da99c11ce75:f5fdcb75@us-cdbr-iron-east-04.cleardb.net/heroku_c8f51469f166c36?reconnect=true",
+					"b92da99c11ce75", "f5fdcb75");
+
+//			return DriverManager.getConnection(
+//                    "jdbc:mysql://localhost/userView", "root", "admin");
 		} catch (SQLException | ClassNotFoundException e) {
 			throw new RuntimeException(e);
 		}
