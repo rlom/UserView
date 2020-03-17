c544334e8a4e13f51165e9b7743229332e006981
Róger Luiz <rlom@cin.ufpe.br>
2020-03-16 22:08:03 -0300

    AJustes finais no banco e na validação de data

--- src/UsuarioController.java
+++ src/UsuarioController.java
@@ -1,5 +1,7 @@
 import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
@@ -58,7 +60,13 @@ public class UsuarioController {
 			context.addMessage(null, new FacesMessage("Info", "Por Favor, preencha um CPF válido!"));
 		}
 
-		if (this.usuario.getNascimento() == null) {
+		Date dataAtual = Calendar.getInstance().getTime();
+		Calendar dataMinima = Calendar.getInstance();
+		dataMinima.set(Calendar.DAY_OF_MONTH, 1);
+		dataMinima.set(Calendar.MONTH, 1);
+		dataMinima.set(Calendar.YEAR, 1900);
+		if (this.usuario.getNascimento() == null || this.usuario.getNascimento().after(dataAtual)
+				|| this.usuario.getNascimento().before(dataMinima.getTime())) {
 			ok = false;
 			context.addMessage(null, new FacesMessage("Info", "Por Favor, preencha uma data de nascimento válido!"));
 		}
@@ -68,7 +76,7 @@ public class UsuarioController {
 			String senha = this.usuario.getCpf().substring(0, 2) + this.usuario.getNascimento().getMonth();
 
 			try {
-				if(usuario.getId() == null) {
+				if (usuario.getId() == null) {
 					MessageDigest digest = MessageDigest.getInstance("SHA-1");
 					digest.reset();
 					digest.update(senha.getBytes("utf8"));
@@ -83,12 +91,12 @@ public class UsuarioController {
 			}
 
 			try {
-				if(usuario.getId() != null) {
+				if (usuario.getId() != null) {
 					dao.editar(usuario);
 				} else {
 					dao.salvar(usuario);
 				}
-				
+
 			} catch (Exception e) {
 				context.addMessage(null, new FacesMessage("Info", "Não foi possível se comunicar com o banco."));
 				return "usuario";
